89ceccb30fa8de0d0606dbe2c04bc07a363d5e51
suphachaiinet <s6506022410031@email.kmutnb.ac.th>
2023-07-11 23:49:21 +0700

    L2

--- L2/examples.js
+++ L2/examples.js
@@ -1,3 +1,6 @@
-function findperimeter(){
-    
+function findperimeter(width,length){
+    return (width*2) + (length*2);
 }
+console.log(findperimeter(6,7));
+console.log(findperimeter(20,10));
+console.log(findperimeter(2,9));
---
+++ L2/examples2.js
@@ -0,0 +1,7 @@
+function equation(value){
+    let x = value
+    return x;
+}
+console.log(equation(parseInt(`${1+1}`)));
+console.log(equation(parseInt(`${7*4-2}`)));
+console.log(equation(parseInt(`${1+1+1+1+1}`)));
