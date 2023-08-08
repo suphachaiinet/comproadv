3e2dbdc1d526c6772c4282eac9457c5d4d4b03b7
suphachaiinet <s6506022410031@email.kmutnb.ac.th>
2023-08-08 09:24:41 +0700

    L5

---
+++ L5/asynccatch.js
@@ -0,0 +1,24 @@
+function yay0rNay() {
+    return new Promise((resolve, reject) => {
+        const val = Math.round(Math.random()*1);
+        val ? resolve("Lucky😖") : reject("Nope 🤑");
+    });
+}
+
+async function msg() {
+    try {
+        const msg = await yay0rNay();
+        console.log(msg);
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+msg();
+msg();
+msg();
+msg();
+msg();
+msg();
+msg();
+
